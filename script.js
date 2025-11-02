af142cf4490eb467cef4165805094ebdebda8181
yupeng-du <ydu6@rrc.ca>
2025-11-01 21:20:12 -0500

    add API search function

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-async function getPermits() {
-  const api = "https://data.winnipeg.ca/resource/it4w-cpf4.json";
-  const response = await fetch(api);
-  const data = await response.json();
-  console.log(data);
-}
+const input = document.querySelector(".search");
+const button = document.querySelector(".search-btn");
+const results = document.querySelector(".results");
 
-getPermits();
+button.addEventListener("click", async function () {
+  const search = input.value;
+  const apiUrl =
+    "https://data.winnipeg.ca/resource/it4w-cpf4.json?" +
+    `$where=lower(address) LIKE lower('%${search}%')`;
+
+  const encodedURL = encodeURI(apiUrl);
+
+  const response = await fetch(encodedURL);
+  const data = await response.json();
+
+  results.innerHTML =
+    data[0].address +
+    "<br>" +
+    data[0].permit_number +
+    "<br>" +
+    data[0].status +
+    "<br>" +
+    data[0].permit_group;
+});
