05f978ec1aae97517e58d63de24617a6c33627c9
yupeng-du <ydu6@rrc.ca>
2025-11-02 00:52:56 -0500

    Fixed to prevent defalut submit.

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const input = document.querySelector(".search");
 const button = document.querySelector(".search-btn");
 const results = document.querySelector(".results");
 
-button.addEventListener("click", async function () {
+button.addEventListener("click", async function (event) {
+  event.preventDefault();
+
   const search = input.value;
   const apiUrl =
     "https://data.winnipeg.ca/resource/it4w-cpf4.json?" +
@@ -23,18 +25,23 @@ button.addEventListener("click", async function () {
       results.innerHTML = "Please check address";
       return;
     }
-    results.innerHTML =
-      "Address: " +
-      data[0].address +
-      "<br>" +
-      "Permit Number: " +
-      data[0].permit_number +
-      "<br>" +
-      "Status: " +
-      data[0].status +
-      "<br>" +
-      "Permit Group: " +
-      data[0].permit_group;
+    results.innerHTML = "";
+
+    for (let i = 0; i < 3 && i < data.length; i++) {
+      results.innerHTML +=
+        "Address: " +
+        data[i].address +
+        "<br>" +
+        "Permit Number: " +
+        data[i].permit_number +
+        "<br>" +
+        "Status: " +
+        data[i].status +
+        "<br>" +
+        "Permit Group: " +
+        data[i].permit_group +
+        "<br><br>";
+    }
   } catch (error) {
     console.error("Error:", error);
     results.innerHTML = "Unexpected error.";
